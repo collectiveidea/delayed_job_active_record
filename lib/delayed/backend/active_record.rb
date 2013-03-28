d151d28acfc46d42cb39f6a704f2e055e31fd3b8
David Genord II <david@collectiveidea.com>
2013-03-28 12:05:15 -0400

    We no longer use lock_exclusively!

--- lib/delayed/backend/active_record.rb
+++ lib/delayed/backend/active_record.rb
@@ -56,28 +56,6 @@ module Delayed
           job
         end
 
-        # Lock this job for this worker.
-        # Returns true if we have the lock, false otherwise.
-        def lock_exclusively!(max_run_time, worker)
-          now = self.class.db_time_now
-          affected_rows = if locked_by != worker
-            # We don't own this job so we will update the locked_by name and the locked_at
-            self.class.update_all(["locked_at = ?, locked_by = ?", now, worker], ["id = ? and (locked_at is null or locked_at < ?) and (run_at <= ?)", id, (now - max_run_time.to_i), now])
-          else
-            # We already own this job, this may happen if the job queue crashes.
-            # Simply resume and update the locked_at
-            self.class.update_all(["locked_at = ?", now], ["id = ? and locked_by = ?", id, worker])
-          end
-          if affected_rows == 1
-            self.locked_at = now
-            self.locked_by = worker
-            self.changed_attributes.clear
-            return true
-          else
-            return false
-          end
-        end
-
         # Get the current time (GMT or local depending on DB)
         # Note: This does not ping the DB to get the time, so all your clients
         # must have syncronized clocks.
