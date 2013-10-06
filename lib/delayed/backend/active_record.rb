9b4af3d8743c3ba04317e08fb0a9e17f47498a7f
Brian Malinconico <bm171y@att.com>
2013-10-06 17:24:45 -0400

    Adding check for deadlock

--- lib/delayed/backend/active_record.rb
+++ lib/delayed/backend/active_record.rb
@@ -73,7 +73,15 @@ module Delayed
           now = self.class.db_time_now
           affected_rows = if locked_by != worker
             # We don't own this job so we will update the locked_by name and the locked_at
-            self.class.update_all(["locked_at = ?, locked_by = ?", now, worker], ["id = ? and (locked_at is null or locked_at < ?) and (run_at <= ?)", id, (now - max_run_time.to_i), now])
+            begin
+              self.class.update_all(["locked_at = ?, locked_by = ?", now, worker], ["id = ? and (locked_at is null or locked_at < ?) and (run_at <= ?)", id, (now - max_run_time.to_i), now])
+            rescue ActiveRecord::StatementInvalid => e
+              if e.message =~ /Deadlock found when trying to get lock/
+                0
+              else
+                raise e
+              end
+            end
           else
             # We already own this job, this may happen if the job queue crashes.
             # Simply resume and update the locked_at
