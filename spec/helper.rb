cabd10d570473537128e18095c301b1fc71f212d
Erik Michaels-Ober <sferik@gmail.com>
2014-06-25 14:30:42 +0200

    Enforce code coverage minimums

--- spec/helper.rb
+++ spec/helper.rb
@@ -5,7 +5,11 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
     SimpleCov::Formatter::HTMLFormatter,
     Coveralls::SimpleCov::Formatter
 ]
-SimpleCov.start
+
+SimpleCov.start do
+  add_filter '/spec/'
+  minimum_coverage(73.33)
+end
 
 require 'logger'
 require 'rspec'
