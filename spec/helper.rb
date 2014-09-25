a53e3cc8c12cc941d8b7467f05a7e057dd9adc1b
David Genord II <david@collectiveidea.com>
2014-09-25 15:13:04 -0400

    Use the migration to setup the test database

--- spec/helper.rb
+++ spec/helper.rb
@@ -33,21 +33,9 @@ ActiveRecord::Base.establish_connection config[db_adapter]
 ActiveRecord::Base.logger = Delayed::Worker.logger
 ActiveRecord::Migration.verbose = false
 
+require 'generators/delayed_job/templates/migration'
 ActiveRecord::Schema.define do
-  create_table :delayed_jobs, force: true do |table|
-    table.integer :priority, default: 0
-    table.integer :attempts, default: 0
-    table.text :handler
-    table.text :last_error
-    table.datetime :run_at
-    table.datetime :locked_at
-    table.datetime :failed_at
-    table.string :locked_by
-    table.string :queue
-    table.timestamps
-  end
-
-  add_index :delayed_jobs, [:priority, :run_at], name: "delayed_jobs_priority"
+  CreateDelayedJobs.up
 
   create_table :stories, primary_key: :story_id, force: true do |table|
     table.string :text
