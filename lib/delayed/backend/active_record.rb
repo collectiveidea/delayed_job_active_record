0d1ed9a8d24015de87e6b7cc6967a4f8b626c8cc
Ryan Lv <ryan.lv@sap.com>
2016-12-27 13:58:06 +0800

    Refactor sql statement
    Before
    
    `(run_at <= ? AND (locked_at IS NULL OR locked_at < ?) OR locked_by =
    ?) AND failed_at IS NULL`
    
    This sql statement is really confusing for sql newbie.
    
    After
    
    `((run_at <= ? AND (locked_at IS NULL OR locked_at < ?)) OR locked_by =
    ?) AND failed_at IS NULL`
    
    `And` has precedence over `Or`. they are equal.
    
    So this change will not break currently behavior. Just make it more plain for sql newbie.
    
    Reviewed by David Genord and Guihere Cavalcanti

--- lib/delayed/backend/active_record.rb
+++ lib/delayed/backend/active_record.rb
@@ -54,7 +54,7 @@ module Delayed
 
         def self.ready_to_run(worker_name, max_run_time)
           where(
-            "(run_at <= ? AND (locked_at IS NULL OR locked_at < ?) OR locked_by = ?) AND failed_at IS NULL",
+            "((run_at <= ? AND (locked_at IS NULL OR locked_at < ?)) OR locked_by = ?) AND failed_at IS NULL",
             db_time_now,
             db_time_now - max_run_time,
             worker_name
